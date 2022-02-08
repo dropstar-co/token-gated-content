f5b91df7c1a623890cde2d20f78fef403ac4fe92
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-02-08 13:27:46 +0100

    Move venly functions to use state for wallets
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- token-gated-content-website/src/TokenGatedContent.js
+++ token-gated-content-website/src/TokenGatedContent.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { Button } from 'react-bootstrap'
 import React, { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
 
 import {
 	VENLY_WIDGET_CLIENT_ID,
@@ -14,51 +15,18 @@ import { useParams } from 'react-router-dom'
 
 const venlyConnect = new VenlyConnect(VENLY_WIDGET_CLIENT_ID, { environment: VENLY_ENVIRONMENT })
 
-async function connect(venlyConnect) {
-	try {
-		const account = await venlyConnect.flows.getAccount(VENLY_CHAIN)
-		console.log(account)
-		postAuth(account.auth, account.wallets)
-	} catch (error) {
-		console.log(error)
-	}
-}
-
-function postAuth(auth, wallets) {
-	console.log(auth.token)
-	hideLoginButton()
-	showUsername(auth)
-	showWallets(wallets)
-}
-
-function hideLoginButton() {
-	const button = document.querySelector('#connect')
-	button.value = 'Already connected'
-	button.disabled = true
-}
-
-function showUsername(auth) {
-	const userName = document.querySelector('#user')
-	userName.textContent = auth.idTokenParsed.nickname
-}
-
-function showWallets(wallets) {
-	const ul = document.createElement('ul')
-	for (const wallet of wallets) {
-		const li = document.createElement('li')
-		li.innerText = `${wallet.description} - ${wallet.address} `
-		ul.appendChild(li)
-	}
-	const body = document.querySelector('body')
-	body.appendChild(ul)
-}
-
 function TokenGatedContent() {
 	const [data, setData] = useState('')
 	const [isAuthenticated, setIsAuthenticated] = useState(false)
+	const [hasTokenGatedContent, setHasTokenGatedContent] = useState(false)
+	const [venlyWallets, setVenlyWallets] = useState([])
 	const params = useParams()
 
-	console.log({ params })
+	const { tokenAddress, tokenId } = params
+
+	const isAddress = ethers.utils.isAddress(tokenAddress)
+
+	console.log(`${tokenAddress} / ${tokenId} --> ${isAddress}`)
 
 	async function checkAuthenticated() {
 		const result = await venlyConnect.checkAuthenticated()
@@ -66,39 +34,97 @@ function TokenGatedContent() {
 			const wallets = await venlyConnect.api.getWallets({ secretType: VENLY_CHAIN })
 			postAuth(auth, wallets)
 			setIsAuthenticated(true)
+			setVenlyWallets(wallets)
+			console.log({ wallets, auth })
 		})
-	}
-
-	async function loadNFTs() {
-		const result = await fetch(TOKEN_GATED_CONTENT_BACKEND_URL + '/nonfungibles')
 	}
 
 	function init() {
 		checkAuthenticated()
 	}
 
+	async function connect(venlyConnect) {
+		try {
+			const account = await venlyConnect.flows.getAccount(VENLY_CHAIN)
+			postAuth(account.auth, account.wallets)
+			setVenlyWallets(account.wallets)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	function postAuth(auth, wallets) {
+		//console.log(auth.token)
+		//console.log({ wallets })
+		hideLoginButton()
+		showUsername(auth)
+		showWallets(wallets)
+	}
+
+	function hideLoginButton() {
+		const button = document.querySelector('#connect')
+		button.value = 'Already connected'
+		button.disabled = true
+	}
+
+	function showUsername(auth) {
+		const userName = document.querySelector('#user')
+		console.log('auth.idTokenParsed.nickname')
+		console.log({ auth })
+		console.log(auth.idTokenParsed.nickname)
+		userName.textContent = auth.idTokenParsed.nickname
+	}
+
+	function showWallets(wallets) {
+		const ul = document.createElement('ul')
+		for (const wallet of wallets) {
+			const li = document.createElement('li')
+			li.innerText = `${wallet.description} - ${wallet.address} `
+			ul.appendChild(li)
+		}
+		const body = document.querySelector('body')
+		body.appendChild(ul)
+	}
+
 	function updateData() {
 		async function asyncFetchData() {
-			const response = await fetch(`${TOKEN_GATED_CONTENT_BACKEND_URL}/status`)
+			const response = await fetch(`${TOKEN_GATED_CONTENT_BACKEND_URL}/generatemessage`, {
+				method: 'POST',
+				body: JSON.stringify({ address: '', tokenAddress, tokenId }),
+			})
 			const json = await response.json()
 			console.log({ json })
+
+			setHasTokenGatedContent(response.status === 200)
 			setData(json.data)
 		}
 		asyncFetchData()
 	}
 
 	useEffect(init, [])
-	useEffect(() => loadNFTs(), [isAuthenticated])
+	useEffect(() => {
+		if (isAuthenticated) updateData()
+	}, [isAuthenticated])
 
 	return (
 		<div>
-			<h1>OTRA PAGINA!!</h1>
+			<div id="user">Log in to see your user name</div>
+			{!isAuthenticated ? (
+				<p>
+					<Button id="connect" onClick={() => connect(venlyConnect)}>
+						Do magic
+					</Button>
+				</p>
+			) : (
+				''
+			)}
 
-			<p>
-				<Button id="connect" onClick={() => connect(venlyConnect)}>
-					Do magic
-				</Button>
-			</p>
+			{hasTokenGatedContent ? (
+				<h1>TokenGatedContent</h1>
+			) : (
+				<h1>404 no token gated content for this token</h1>
+			)}
+
 			<p>{JSON.stringify(data, null, 2)}</p>
 			<p>{isAuthenticated ? 'Authenticated' : 'Not authenticated'}</p>
 		</div>
